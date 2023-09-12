3dc75eeab4de9206f3e3c14ba258432ef514ee1a
VA <avs.mailbox@yahoo.com>
2023-09-12 16:18:12 +0300

    Getting started (React basic frontend).

---
+++ frontend/react/src/assets/index.js
@@ -0,0 +1,8 @@
+import loader from './loader.svg';
+import logo from './logo.svg';
+// import logo from './logo.png';
+
+export {
+  logo,
+  loader,
+};
---
+++ frontend/react/src/pages/SignIn.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SignIn = () => (
+  <>
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Sign in
+      </h2>
+
+    </div>
+
+    <div />
+  </>
+);
+
+export default SignIn;
