2ef3e2bd1a64582e498920d48169cd20a5e4f4e4
VA <avs.mailbox@yahoo.com>
2023-09-25 13:02:02 +0300

    Testing UserRowMapper

---
+++ user-service/src/test/java/com/musicclouds/user/dao/UserRowMapperTest.java
@@ -0,0 +1,38 @@
+package com.musicclouds.user.dao;
+
+import com.musicclouds.user.AbstractTestcontainers;
+import com.musicclouds.user.domain.User;
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserRowMapperTest extends AbstractTestcontainers {
+
+    @Test
+    void mapRow() throws SQLException {
+        // Given
+        UserRowMapper userRowMapper = new UserRowMapper();
+
+        ResultSet resultSet = mock(ResultSet.class);
+        when(resultSet.getInt("id")).thenReturn(1);
+        when(resultSet.getString("first_name")).thenReturn("Jamila");
+        when(resultSet.getString("last_name")).thenReturn("Jason");
+        when(resultSet.getString("email")).thenReturn("jamila@gmail.com");
+        when(resultSet.getString("username")).thenReturn("Jamila19");
+
+        // When
+        User actual = userRowMapper.mapRow(resultSet, 1);
+
+        // Then
+        User expected = new User(
+                1, "Jamila", "Jason", "jamila@gmail.com", "Jamila19"
+        );
+        assertThat(actual).isEqualTo(expected);
+    }
+}
